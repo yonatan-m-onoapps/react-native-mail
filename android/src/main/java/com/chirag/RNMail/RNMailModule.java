a4a729168be176114b4b74d55c2edf0adceeb357
Adam Wilson <adam@codegarden.co.uk>
2018-05-15 19:10:52 +0100

    Add missing file copy method

--- android/src/main/java/com/chirag/RNMail/RNMailModule.java
+++ android/src/main/java/com/chirag/RNMail/RNMailModule.java
@@ -153,4 +153,23 @@ public class RNMailModule extends ReactContextBaseJavaModule {
 
     }
   }
+  
+  protected static void copy(File src, File dst) throws IOException {
+    InputStream in = new FileInputStream(src);
+    try {
+      OutputStream out = new FileOutputStream(dst);
+      try {
+        // Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+          out.write(buf, 0, len);
+        }
+      } finally {
+        out.close();
+      }
+    } finally {
+      in.close();
+    }
+  }
 }
