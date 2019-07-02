0fdeb3b148c198c5f5b2f4668dce5ffa832bc269
yonatan-m-onoapps <44137117+yonatan-m-onoapps@users.noreply.github.com>
2019-07-02 16:15:15 +0300

    Migration to androidX

--- android/src/main/java/com/chirag/RNMail/RNMailModule.java
+++ android/src/main/java/com/chirag/RNMail/RNMailModule.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 import android.text.Html;
 import android.util.Log;
 
